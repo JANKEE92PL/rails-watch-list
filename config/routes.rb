a8dfcf4db53611c50c6559a7ffd0b9b99f1c7784
JANKEE92PL <jankee112@freenet.de>
2022-01-24 14:10:28 +0100

    create Movies Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-
+  get 'movies/new'
+  get 'movies/create'
+  get 'movies/update'
+  get 'movies/edit'
+  get 'movies/destroy'
+  get 'movies/index'
+  get 'movies/show'
+  resources :movies
+  resources :lists
+  resources :bookmarks
 end
